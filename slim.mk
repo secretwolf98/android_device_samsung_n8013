06b2717f9845c396f6ee7dfdd84a81ea201ec35a
Secretwolf98 <jordanluders@gmail.com>
2020-01-23 13:50:26 -0600

    Test out for Slim7.

--- slim.mk
+++ slim.mk
@@ -7,12 +7,12 @@ $(call inherit-product, device/samsung/n8013/n8013.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+# Inherit some common SlimROMS stuff.
+$(call inherit-product, vendor/slim/config/common_full_tablet_wifionly.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := n8013
-PRODUCT_NAME := lineage_n8013
+PRODUCT_NAME := slim_n8013
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-N8013
 PRODUCT_MANUFACTURER := samsung
@@ -21,5 +21,5 @@ PRODUCT_MANUFACTURER := samsung
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=p4notewifiue \
 	TARGET_DEVICE=p4notewifi \
-	BUILD_FINGERPRINT="LineageOS/Samsung/${PRODUCT_MODEL}/${PRODUCT_DEVICE}/${PLATFORM_VERSION}/${BUILD_ID}/userdebug/test-keys" \
+	BUILD_FINGERPRINT="SlimROMS/Samsung/${PRODUCT_MODEL}/${PRODUCT_DEVICE}/${PLATFORM_VERSION}/${BUILD_ID}/userdebug/test-keys" \
 	PRIVATE_BUILD_DESC="n8013-userdebug 7.1.2 NJH47F cd81f4c40e test-keys"
